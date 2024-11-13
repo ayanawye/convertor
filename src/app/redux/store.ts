645c27ec72bbee1f1839e353a18ecb2b34dd131d
ayana_kai <kaichybekovaayana@gmail.com>
2024-11-13 18:13:58 +0600

    config store

--- src/app/redux/store.ts
+++ src/app/redux/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { currencySlice } from "../../entities/currency";
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    allCurrencies: currencySlice.reducer,
+  },
 });
 
 export default store;
